f6013a5117db549594df44abb9aa7607a29ee045
Mex <mexuk@hotmail.co.uk>
2021-09-18 20:04:21 +0100

    resource commands

--- resources/gtac-essentials/code/scripts/resources/ResourcesS.js
+++ resources/gtac-essentials/code/scripts/resources/ResourcesS.js
@@ -4,7 +4,7 @@ global.resources = {};
 cmds.resources = (client) =>
 {
 	var resources = getResources();
-	chat.all('There ' + util.isAre(resources.length) + ' ' + resources.length + ' ' + util.plural('resoucre', resources.length) + ' running.');
+	chat.all('There ' + util.isAre(resources.length) + ' ' + resources.length + ' ' + util.plural('resoucre', resources.length) + '.');
 };
 
 cmds.resourcenames = (client) =>
@@ -14,5 +14,53 @@ cmds.resourcenames = (client) =>
 		chat.all("There aren't any resources running.");
 	else
 		chat.all('Resource Names: ' + resources.map(resource => resource.name).join(', '));
+};
+
+cmds.resourcesstarted = (client) =>
+{
+	var resources = [];
+	getResources().map(resource =>
+	{
+		if(resource.isStarted)
+		{
+			resources.push(resource);
+		}
+	});
+	if(resources.length == 0)
+		chat.all("There aren't any resources started.");
+	else
+		chat.all('Resources Started: ' + resources.map(resource => resource.name).join(', '));
+};
+
+cmds.resourcesstopped = (client) =>
+{
+	var resources = [];
+	getResources().map(resource =>
+	{
+		if(!resource.isStarted)
+		{
+			resources.push(resource);
+		}
+	});
+	if(resources.length == 0)
+		chat.all("There aren't any resources started.");
+	else
+		chat.all('Resources Started: ' + resources.map(resource => resource.name).join(', '));
+};
+
+cmds.resourcesstarting = (client) =>
+{
+	var resources = [];
+	getResources().map(resource =>
+	{
+		if(resource.isStarting)
+		{
+			resources.push(resource);
+		}
+	});
+	if(resources.length == 0)
+		chat.all("There aren't any resources starting.");
+	else
+		chat.all('Resources Starting: ' + resources.map(resource => resource.name).join(', '));
 };
 
