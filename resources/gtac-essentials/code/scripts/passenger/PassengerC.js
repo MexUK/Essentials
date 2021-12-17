0444a7ab40a47c1de41e8976fb5af400266dab69
Mex <mexuk@hotmail.co.uk>
2021-12-17 23:51:03 +0000

    Tidy

--- resources/gtac-essentials/code/scripts/passenger/PassengerC.js
+++ resources/gtac-essentials/code/scripts/passenger/PassengerC.js
@@ -10,28 +10,10 @@ addEventHandler('onKeyDown', (event,key) =>
 
 passenger.enterNearestVehicle = () =>
 {
-	var vehicle = passenger.getNearestVehicle();
+	var vehicle = util.getNearestVehicle(localPlayer.position);
 	if(!vehicle)
 		return;
 	
 	localPlayer.enterVehicle(vehicle, false);
-};
-
-passenger.getNearestVehicle = () =>
-{
-	var vehicles = getVehicles();
-	var localPlayerPosition = localPlayer.position;
-	var nearestVehicle = null;
-	var vehDis, nearestVehDis = 99999.999;
-	for(var i=0,j=vehicles.length; i<j; i++)
-	{
-		vehDis = vehicles[i].position.distance(localPlayerPosition);
-		if(vehDis < nearestVehDis)
-		{
-			nearestVehDis = vehDis;
-			nearestVehicle = vehicles[i];
-		}
-	}
-	return nearestVehicle;
 };
 
